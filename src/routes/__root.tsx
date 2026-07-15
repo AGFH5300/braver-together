cb90c9efd1f3d29eda57d6d9ccc018ac9c3710ac
AGFH5300 <dxb.avg@gmail.com>
2026-07-15 20:41:05 +0530

    fix: mount the global toast notification UI

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,6 +9,7 @@ import {
 } from "@tanstack/react-router";
 import { useEffect, type ReactNode } from "react";
 
+import { Toaster } from "@/components/ui/sonner";
 import appCss from "../styles.css?url";
 import { reportClientError } from "../lib/error-reporting";
 
@@ -117,6 +118,7 @@ function RootComponent() {
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
+      <Toaster richColors closeButton position="top-right" />
     </QueryClientProvider>
   );
 }
